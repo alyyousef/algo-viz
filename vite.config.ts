708b704005bbdf8a9838a1a970070aa63ec380d3
Ali <aliyousef.aca@gmail.com>
2026-02-16 14:48:49 +0200

    Port number change

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 8889,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
